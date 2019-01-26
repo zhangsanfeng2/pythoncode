667a0ddee00bb88f1a385a814dea5df9618842de
wy <wy@126.com>
2019-01-26 12:45:18 +0800

    rrr

--- hello.py
+++ hello.py
@@ -1,14 +1,11 @@
 def sayhi(name):
     print("hello world",name)
     print("okkkk")
-<<<<<<< HEAD
     print("sdfjksjfd")
     print("change dev")
-=======
     print("bug-fix100")
 
 
 def test():
 
     bug-fix-100
->>>>>>> bug-fix-100
